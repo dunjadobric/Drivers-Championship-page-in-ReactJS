2750f0f6f1f60a0a1e7ba8bb3bd06466d770a961
DunjaDobric <dunjadobric@yahoo.com>
2021-06-28 09:55:09 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./css/style.css";
+//import "./scss/style.scss";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import * as $ from "jquery";
+var ReactCSSTransitionGroup = require("react-addons-css-transition-group");
+import { PropTypes } from "prop-types";
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
