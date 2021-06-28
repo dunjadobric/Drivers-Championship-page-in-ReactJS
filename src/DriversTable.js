d11b09f3a3dbccb634825174ae6d8bd77e2bf2a2
DunjaDobric <dunjadobric@yahoo.com>
2021-06-28 16:09:37 +0200

    Setovan drivers

--- src/DriversTable.js
+++ src/DriversTable.js
@@ -1,15 +1,48 @@
 import React from "react";
+import * as $ from "jquery";
 
 export default class DriversTable extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            drivers: []
+        }
+    }
+
+    componentDidMount() {
+        this.getResponse();
+    }
+
+    getResponse() {
+        var url = "http://ergast.com/api/f1/2013/driverStandings.json";
+        $.get(url, (data) => {
+            console.log(data);
+            this.setState({
+                drivers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+            })
+        })
     }
 
     render() {
         return(
-            <div>
-                Drivers
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Constructors Championships Standings - 2013</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.drivers.map((driver, i) => {
+                        return(
+                            <tr key={i}>
+                                <td>{driver.position}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         )
     }
 }
+
