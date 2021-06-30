4df89fd130d115c783c2780cd2120a22adbd5df5
BojanFilipovic1974 <bojan.filipovic021ns@gmail.com>
2021-06-30 08:31:11 +0200

    Formula

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default class App extends React.Component {
                                 </li>
                                 <li className="link">
                                     <Link to="/teams">
-                                        <img src="img/timovi.png"/>
+                                        <img src="img/timovi1.png"/>
                                         <p>Teams</p>
                                         </Link>
                                 </li>
