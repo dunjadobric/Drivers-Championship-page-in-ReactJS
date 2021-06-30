165b1bcffcc5a43e2f7d56aee26c98f8a47a83ba
duleloncar <duleloncar.ns@gmail.com>
2021-06-30 15:56:08 +0200

    driversDetail table and card

--- src/DriverDetails.js
+++ src/DriverDetails.js
@@ -2,126 +2,281 @@ import React from "react";
 import { Link } from "react-router-dom";
 import * as $ from "jquery";
 import Flag from "react-flagkit";
+import Races from "./Races";
 
 export default class DriverDetails extends React.Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      drivers: [],
-      flags: [],
-    };
-  }
-  componentDidMount() {
-    
-    this.getDrivers();
-    this.getFlags();
-  }
+		this.state = {
+			drivers: [],
+			flags: [],
+			races: [],
+		};
+	}
+	componentDidMount() {
+		this.getDrivers();
+		this.getFlags();
+		this.getRaces();
+	}
 
-  getDrivers() {
-    const id = this.props.match.params.id;
-    console.log(id);
-    var url ="http://ergast.com/api/f1/2013/drivers/" + id + "/driverStandings.json";
-    $.get(url, (data) => {
-      console.log(data);
-      this.setState({
-        drivers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings,
-      });
-    });
-  }
-  getFlags() {
-    var url =
-      "https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json";
+	getDrivers() {
+		const id = this.props.match.params.id;
+		console.log(id);
+		var url =
+			"http://ergast.com/api/f1/2013/drivers/" +
+			id +
+			"/driverStandings.json";
+		$.get(url, (data) => {
+			console.log(data);
+			this.setState({
+				drivers:
+					data.MRData.StandingsTable.StandingsLists[0]
+						.DriverStandings,
+			});
+		});
+	}
+	getFlags() {
+		var url =
+			"https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json";
 
-    $.get(url, (data) => {
-      var flags = JSON.parse(data);
-      this.setState({
-        flags: flags,
-      });
-    });
-  }
+		$.get(url, (data) => {
+			var flags = JSON.parse(data);
+			this.setState({
+				flags: flags,
+			});
+		});
+	}
 
-  render() {
-      console.log(this.state.drivers)
-    return ( 
-    <div>
-        {this.state.drivers.map((driver, i)=>{
-            return(
-                <div key={i}>
-            <div>
-                <img src="../img/drivers/Sebastian_Vettel.jpg"/>
-            </div>
-            <div>
-                <div>
-                {this.state.flags.map((flag, i) => {
-                            if (
-                              driver.Driver.nationality === "British" &&
-                              flag.nationality === "British, UK"
-                            ) {
-                              return <Flag key={i} country="GB" />;
-                            } else if (
-                              driver.Driver.nationality === "Dutch" &&
-                              flag.nationality === "Dutch, Netherlandic"
-                            ) {
-                              return <Flag key={i} country="NL" />;
-                            } else {
-                              if (
-                                driver.Driver.nationality === flag.nationality
-                              ) {
-                                return (
-                                  <Flag key={i} country={flag.alpha_2_code} size={60}/>
-                                );
-                              }
-                            }
-                          })}
-                </div>
-               
-           
-            </div>
+	getRaces() {
+		const id = this.props.match.params.id;
+		var url =
+			"http://ergast.com/api/f1/2013/drivers/" + id + "/results.json";
+		$.get(url, (data) => {
+			console.log(data);
+			this.setState({
+				races: data.MRData.RaceTable.Races,
+			});
+		});
+	}
 
-            <table>
+	render() {
+		console.log(this.state.drivers);
+		console.log(this.state.races);
+		return (
+			<div className="driverDetails">
+				<div className="driverCard">
+					{this.state.drivers.map((driver, i) => {
+						return (
+							<div key={i}>
+								<div className="driverHeading">
+									<div className="driverPhoto">
+										<img src="../img/drivers/Sebastian_Vettel.jpg" />
+									</div>
+									<div>
+										<div>
+											{this.state.flags.map((flag, i) => {
+												if (
+													driver.Driver
+														.nationality ===
+														"British" &&
+													flag.nationality ===
+														"British, UK"
+												) {
+													return (
+														<Flag
+															key={i}
+															country="GB"
+															size={60}
+														/>
+													);
+												} else if (
+													driver.Driver
+														.nationality ===
+														"Dutch" &&
+													flag.nationality ===
+														"Dutch, Netherlandic"
+												) {
+													return (
+														<Flag
+															key={i}
+															country="NL"
+															size={60}
+														/>
+													);
+												} else {
+													if (
+														driver.Driver
+															.nationality ===
+														flag.nationality
+													) {
+														return (
+															<Flag
+																key={i}
+																country={
+																	flag.alpha_2_code
+																}
+																size={60}
+															/>
+														);
+													}
+												}
+											})}
+										</div>
+										<div className="driverName">
+											<div>{driver.Driver.givenName}</div>
+											<div>
+												{driver.Driver.familyName}
+											</div>
+										</div>
+									</div>
+								</div>
 
-                <tr>
-                    <th colSpan="2">{driver.Driver.givenName}</th>
-                    
-                </tr>
-                <tr>
-                    <th colSpan="2">{driver.Driver.familyName}</th>
-                    
-                </tr>
-                <tr>
-                    <th>Country:</th>
-                    <td>{driver.Driver.nationality}</td>
-                </tr>
-                <tr>
-                    <th>Team:</th>
-                    <td>{driver.Constructors[0].name}</td>
-                </tr>
-                <tr>
-                    <th>Birth:</th>
-                    <td>{driver.Driver.dateOfBirth}</td>
-                </tr>
-                <tr>
-                    <th colSpan="2"><Link to="#">Biography</Link></th>
-                </tr>
-                
-                </table> 
-
-           
-
-        </div>
-            )
-        })}
-        
-    </div>
-  )
-  }
+								<table className="driverCardTable">
+									<tr>
+										<th>Country:</th>
+										<td>{driver.Driver.nationality}</td>
+									</tr>
+									<tr>
+										<th>Team:</th>
+										<td>{driver.Constructors[0].name}</td>
+									</tr>
+									<tr>
+										<th>Birth:</th>
+										<td>{driver.Driver.dateOfBirth}</td>
+									</tr>
+									<tr>
+										<th>Biography:</th>
+										<td>
+											<Link to="#">
+												<i class="fa fa-external-link"></i>
+											</Link>
+										</td>
+									</tr>
+								</table>
+							</div>
+						);
+					})}
+				</div>
+				<div class="driverDetailsTable">
+					<table>
+						<thead>
+							<tr>
+								<th colSpan="5">Formula 1 2013 Results</th>
+							</tr>
+							<tr>
+								<th>Round</th>
+								<th>Grand Prix</th>
+								<th>Team</th>
+								<th>Grid</th>
+								<th>Race</th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.state.races.map((race, i) => {
+								return (
+									<tr key={i}>
+										<td className="position">
+											{race.round}
+										</td>
+										<td className="constructorRaces">
+											<Link to="#">
+												{this.state.flags.map(
+													(flag, i) => {
+														if (
+															race.Circuit
+																.Location
+																.country ===
+																"UK" &&
+															flag.en_short_name ===
+																"United Kingdom of Great Britain and Northern Ireland"
+														) {
+															return (
+																<Flag
+																	key={i}
+																	country="GB"
+																	size={30}
+																/>
+															);
+														} else if (
+															race.Circuit
+																.Location
+																.country ===
+																"Korea" &&
+															flag.en_short_name ===
+																"Korea (Republic of)"
+														) {
+															return (
+																<Flag
+																	key={i}
+																	country="KR"
+																/>
+															);
+														} else if (
+															race.Circuit
+																.Location
+																.country ===
+																"UAE" &&
+															flag.en_short_name ===
+																"United Arab Emirates"
+														) {
+															return (
+																<Flag
+																	key={i}
+																	country="AE"
+																/>
+															);
+														} else if (
+															race.Circuit
+																.Location
+																.country ===
+																"USA" &&
+															flag.en_short_name ===
+																"United States of America"
+														) {
+															return (
+																<Flag
+																	key={i}
+																	country="US"
+																/>
+															);
+														} else {
+															if (
+																race.Circuit
+																	.Location
+																	.country ===
+																flag.en_short_name
+															) {
+																return (
+																	<Flag
+																		key={i}
+																		country={
+																			flag.alpha_2_code
+																		}
+																		size={
+																			30
+																		}
+																	/>
+																);
+															}
+														}
+													}
+												)}
+												<p>{race.raceName}</p>
+											</Link>
+										</td>
+										<td>
+											{race.Results[0].Constructor.name}
+										</td>
+										<td>{race.Results[0].grid}</td>
+										<td>{race.Results[0].position}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		);
+	}
 }
-
-
-
-
-
-
-
-
