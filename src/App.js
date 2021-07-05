06decc185b63ad020b496a5864347164cc6d6bf9
DunjaDobric <dunjadobric@yahoo.com>
2021-07-05 09:02:30 +0200

    Uvezan scss u App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./css/style.css";
+//import "./css/style.css";
+import "./scss/style.scss";
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import * as $ from "jquery";
 var ReactCSSTransitionGroup = require("react-addons-css-transition-group");
